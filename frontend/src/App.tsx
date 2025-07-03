c3c63db5b8eaf1caa89bee1f951106671590ee1f
Sung Hoon Kim <156278098+tony96kimsh@users.noreply.github.com>
2025-07-03 11:08:23 +0900

    feat: 공통 Update App로 수정하였습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Layout/Header';
-import LoginPage from './pages/LoginPage';
-import AttendancePage from './pages/attendance/AttendancePage'; // 경로 확인 필요
-import AttendanceDetailPage from './pages/attendance/AttendanceDetailPage';
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Header from './components/Layout/Header'
+import LoginPage from './pages/LoginPage'
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/attend" element={<AttendancePage />} />
-        <Route path="/attend/:id" element={<AttendanceDetailPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <LoginPage />
+      </BrowserRouter>
+      
+    </>
   );
 }
 
