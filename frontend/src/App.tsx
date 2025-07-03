406a0cd497e877132d343a1bd8f53d27f60cc876
hanseok0621 <hanseok@gmail.com>
2025-07-03 18:01:29 +0900

    refactor: 김성훈 트롤

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/auth/LoginPage';
-import AdminHome from './pages/home/AdminHome';
-import ManagerHome from './pages/home/ManagerHome';
-import EmployeeHome from './pages/home/EmployeeHome';
-import UnauthorizedPage from './pages/auth/UnauthorizedPage';
-import ProtectedRoute from './components/auth/ProtectedRoute';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import AppRoutes from './routes';
+
+const RoutesRenderer = () => {
+  const element = useRoutes(AppRoutes);
+  return element;
+};
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-
-        {/* 관리자 전용 */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute requiredRoles={['Admin']}>
-              <AdminHome />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 팀장 전용 */}
-        <Route
-          path="/manager"
-          element={
-            <ProtectedRoute requiredRoles={['Manager']}>
-              <ManagerHome />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 직원 전용 */}
-        <Route
-          path="/employee"
-          element={
-            <ProtectedRoute requiredRoles={['Employee']}>
-              <EmployeeHome />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 권한 없음 */}
-        <Route path="/unauthorized" element={<UnauthorizedPage />} />
-                
-      </Routes>
+      <RoutesRenderer />
     </Router>
   );
 }
