e246231be4945a4dbb0196aef97380ce79292882
tony96kimsh <tony96kimsh@gmail.com>
2025-07-03 19:16:23 +0900

    refactor: 라우트 연결 및 임시  proectedRoudte 주석 처리 refactor: update AttendanceRoutes to improve structure and readability

--- frontend/src/routes/AttendanceRoutes.tsx
+++ frontend/src/routes/AttendanceRoutes.tsx
@@ -1,7 +1,30 @@
 import type { RouteObject } from "react-router-dom";
+// import ProtectedRoute from "../components/auth/ProtectedRoute";
+import Layout from "../components/layout/Layout";
+import AttendancePage from "../pages/attendance/AttendancePage";
+import AttendanceDetailPage from "../pages/attendance/AttendanceDetailPage";
 
 const AttendanceRoutes: RouteObject[] = [
-    
+  {
+    path: '/attend',
+    element: (
+      // <ProtectedRoute>
+        <Layout>
+          <AttendancePage />
+        </Layout>
+      // </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/attend/:id',
+    element: (
+      // <ProtectedRoute>
+        <Layout>
+          <AttendanceDetailPage />
+        </Layout>
+      // </ProtectedRoute>
+    ),
+  },
 ];
 
 export default AttendanceRoutes;
